edeee915e25f675cacd1cb4794226a5caf576524
Thomas Orten <thomas.orten@gmail.com>
2014-05-21 17:34:21 +0200

    Initial Commit

---
+++ SantasChoice/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SantasChoice
+//
+//  Created by Orten, Thomas on 21.05.14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SantasChoice/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SantasChoice
+//
+//  Created by Orten, Thomas on 21.05.14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
